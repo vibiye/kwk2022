06362d41843c6f87026985dd60f36bd7eab98f04
vibiye <110997540+vibiye@users.noreply.github.com>
2022-08-12 14:59:57 -0400

    Add files via upload

---
+++ kwk projects/toDoList/toDoList/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  toDoList
+//
+//  Created by scholar on 8/12/22.
+//
+
+import UIKit
+
+class ToDoClass {
+    var description = ""
+    var important = false
+}
---
+++ kwk projects/toDoList/toDoList/ToDoTableViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ToDoTableViewController.swift
+//  toDoList
+//
+//  Created by scholar on 8/12/22.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+
+    var listOfToDo : [ToDoClass] = []
+    func createToDo() -> [ToDoClass] {
+        let swiftToDo = ToDoClass()
+        swiftToDo.description = "Learn Swift"
+        swiftToDo.important = true
+        
+        let dogToDo = ToDoClass()
+        dogToDo.description = "Walk the Dog"
+        return [swiftToDo, dogToDo]
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        listOfToDo = createToDo()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 2
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+        let eachToDo = listOfToDo[indexPath.row]
+        
+        if eachToDo.important {
+            cell.textLabel?.text = "❗️" + eachToDo.description
+        } else {
+            cell.textLabel?.text = eachToDo.description
+        }
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
